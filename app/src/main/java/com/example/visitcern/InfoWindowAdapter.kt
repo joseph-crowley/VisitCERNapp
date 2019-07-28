b0dd672b7b34f38e19bbc2f925c33ea14b5e2c6a
Ryan Nevils <rnevils@calpoly.edu>
2019-07-28 11:26:29 +0200

    changed font size

--- app/src/main/java/com/example/visitcern/InfoWindowAdapter.kt
+++ app/src/main/java/com/example/visitcern/InfoWindowAdapter.kt
@@ -31,7 +31,10 @@ class InfoWindowAdapter(context: Context) : GoogleMap.InfoWindowAdapter {
         val tvDetails = v.findViewById<View>(R.id.tvd) as TextView
         // TextView tvLng = (TextView) v.findViewById(R.id.tv_lng);
         tvGir.text = "This is the Globe"
-        tvDetails.text = "Yeah its the globe"
+        tvDetails.text = "27 metres high and 40 metres in diameter, it's about the size of the dome of Saint Peter's in Rome! A unique visual landmark by day and by night, the Globe of Science and Innovation is a symbol of Planet Earth. It is CERN's outreach tool for its work in the fields of science, particle physics, leading-edge technologies and their applications in everyday life.\n" +
+                "On the ground floor of the Globe, the \"Universe of Particles\" exhibition takes the visitor on a journey deep into the world of particles and back to the Big Bang.\n" +
+                "The free exhibition is for visitors to confront the great questions of contemporary physics, currently being explored by the CERN via the LHC and other accelerators.\n" +
+                "Dive into the fascinating world of particles.\n"
         //tvLng.setText("Longitude:"+ latLng.longitude);
         return v
     }

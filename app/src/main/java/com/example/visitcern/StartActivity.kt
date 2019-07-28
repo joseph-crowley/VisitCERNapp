4a25277e9d54cbb863b1d72331798f54ca521a7c
Andreas Molander <andreas.molander@cern.ch>
2019-07-28 08:55:46 +0200

    Remove extra header bar and email button

--- app/src/main/java/com/example/visitcern/StartActivity.kt
+++ app/src/main/java/com/example/visitcern/StartActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
-import com.google.android.material.snackbar.Snackbar
+//import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 
 import kotlinx.android.synthetic.main.activity_start.*
